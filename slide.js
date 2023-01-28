252e25a0500c76e7a87531a3641c22d5daaaf227
yusupfadilah65 <112670171+yusupfadilah65@users.noreply.github.com>
2023-01-28 13:41:06 +0700

    Add files via upload

---
+++ slide.js
@@ -0,0 +1,11 @@
+//SLIDE 1
+let slide1atas = ("YUSUP STORE")
+let slide1bawah = ("Deskripsi Slidenyaa")
+
+//SLIDE 2
+let slide2atas = ("YUSUP STORE")
+let slide2bawah = ("Deskripsi Slidenyaa")
+
+//SLIDE 3
+let slide3atas = ("YUSUP STORE")
+let slide3bawah = ("Deskripsi Slidenyaa")
